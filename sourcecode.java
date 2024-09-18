79e3b4803d557a7e067c121f040d7dba6d8cf32e
root <root@ip-172-31-46-19.ec2.internal>
2024-09-18 16:13:16 +0000

    first commit

---
+++ sourcecode.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+   
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
+
+    }
+}
+
+
